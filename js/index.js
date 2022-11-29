147f81393bf2f552c3864bf382429ababb1c83c1
lukefrostwalker <116066162+lukefrostwalker@users.noreply.github.com>
2022-11-29 13:51:46 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,147 @@
+const selectDate = document.querySelectorAll("#dateSelector")
+let planContainer = document.querySelector("#planContainer")
+let dateContainer = document.querySelector("#dateContainer")
+
+var menu = [
+    ["Cocoa-spiced Beef Tenderloin",
+     "Latin American flavors come alive in this festive beef dish with fruity salsa.", 
+     "$49.99",
+     `Each serving provides:<br/>
+    <b>calories</b> 215;
+    <b>total fat</b> 9 g;
+    <b>saturated fat</b> 3 g;
+    <b>cholesterol</b> 67 mg;
+    <b>sodium</b> 226 mg;
+    <b>total fiber</b> 2 g;
+    <b>protein</b> 25 g;
+    <b>carbohydrates</b> 9 g;
+    <b>potassium</b> 451 mg;`, "../img/cocoa-spiced beef tenderloin.png"],
+    ["Stir-Fried Orange Beef",
+     "Tangy orange-flavored beef with crips vegetables.<br/>&nbsp;", 
+     "$69.99", 
+     `Each serving provides:<br/>
+    <b>calories</b> 261;
+    <b>total fat</b> 9 g;
+    <b>saturated fat</b> 2 g;
+    <b>cholesterol</b> 28 mg;
+    <b>sodium</b> 418 mg;
+    <b>total fiber</b> 3 g;
+    <b>protein</b> 23 g;
+    <b>carbohydrates</b> 23 g;
+    <b>potassium</b> 648 mg;`, "../img/cocoa-spiced beef tenderloin.png"],
+    ["Sweet-and-Sour Chicken", 
+     "Sweet and sour flavours make a winning combination in this healthier version of a popular Chinese dish.", 
+     "$39.99",
+     `Each serving provides:<br/>
+    <b>calories</b> 221;
+    <b>total fat</b> 6 g;
+    <b>saturated fat</b> 1 g;
+    <b>cholesterol</b> 51 mg;
+    <b>sodium</b> 287 mg;
+    <b>total fiber</b> 3 g;
+    <b>protein</b> 23 g;
+    <b>carbohydrates</b> 21 g;
+    <b>potassium</b> 460 mg;`, "../img/cocoa-spiced beef tenderloin.png"]
+]
+
+const dateClicked = e => {
+    let date = e.target.dataset.date
+    e.target.style.color = "black"
+    e.target.classList = "hello"
+    console.log(date)
+
+    
+    const menuCard = () => {
+        menu.forEach(p => {
+            mealGroup.innerHTML += `
+            
+            <div class="card border border-secondary">
+                    <img src="${p[4]}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${p[0]}</h5>
+                    <p class="card-text">${p[1]}</p>
+                </div>
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item small">Price: <span class="fw-bold" id="price">${p[2]}</span></li>
+                    <li class="list-group-item small">${p[3]}
+                    </li>
+                    </ul>
+                <div class="card-body">
+                    <label for="quantity">Quantity: </label>
+                    <input type="number" class="small" id="quantity" min="1">
+                </div>
+            </div>`
+        })
+    }
+
+    let main = document.createElement("div")
+    main.classList = "mb-3"
+
+    let dateTitle = document.createElement("p")
+    dateTitle.classList = "date_title fw-bold text-center py-2"
+    dateTitle.innerHTML = date + " MENU"
+
+    let mealGroup = document.createElement("div")
+    mealGroup.classList = "card-group gap-4"
+
+    planContainer.appendChild(main)
+    main.appendChild(dateTitle)
+    main.appendChild(mealGroup)
+    mealGroup.appendChild(menuCard())
+ 
+}
+
+for (let x of selectDate) {
+    x.addEventListener("click", dateClicked)
+}
+
+let content = ""
+// MENU CARD
+
+
+// const dateClicked = e => {
+//     let date = e.target.dataset.value
+//     console.log(date)
+
+//     let dateTitle = document.createElement("span")
+//     dateTitle.classList = "date_title fw-bold text-center"
+//     dateTitle.innerHTML = date
+
+//     let mealGroup = document.createElement("div")
+//     mealGroup.classList = "card-group"
+
+//     planContainer.appendChild(dateTitle)
+//     // mealGroup.appendChild(menuCard())
+//     menuCard()
+ 
+
+// }
+
+// for (let x of selectDate) {
+//     x.addEventListener("click", dateClicked)
+// }
+
+// let content = ""
+// // MENU CARD
+// const menuCard = () => {
+//     menu.forEach(p => {
+//         planContainer.innerHTML += `
+        
+//         <div class="card col-3">
+//                 <img src="${p[4]}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//                 <h5 class="card-title">${p[0]}</h5>
+//                 <p class="card-text">${p[1]}</p>
+//             </div>
+//                 <ul class="list-group list-group-flush">
+//                 <li class="list-group-item small">Price: <span class="fw-bold" id="price">${p[2]}</span></li>
+//                 <li class="list-group-item small">${p[3]}
+//                 </li>
+//                 </ul>
+//             <div class="card-body d-flex">
+//                 <label for="quantity">Quantity: </label>
+//                 <input type="number" class="small" id="quantity" min="1">
+//             </div>
+//         </div>`
+//     })
+// }
