9f3bb180c1d38ef6c4c42aafe239bd8d06345a09
lukefrostwalker <116066162+lukefrostwalker@users.noreply.github.com>
2022-12-03 03:33:18 +0800

    .

--- js/index.js
+++ js/index.js
@@ -2,78 +2,42 @@ const selectDate = document.querySelectorAll("#dateSelector");
 let planContainer = document.querySelector("#planContainer");
 let dateContainer = document.querySelector("#dateContainer");
 let totalAmount = document.querySelector("#totalAmount");
+let cartContainer = document.querySelector("#cart")
 
-var menu = [
-    ["Cocoa-spiced Beef Tenderloin",
-     "Latin American flavors come alive in this festive beef dish with fruity salsa.", 
-     "$49.99",
-     `Each serving provides:<br/>
-    <b>calories</b> 215;
-    <b>total fat</b> 9 g;
-    <b>saturated fat</b> 3 g;
-    <b>cholesterol</b> 67 mg;
-    <b>sodium</b> 226 mg;
-    <b>total fiber</b> 2 g;
-    <b>protein</b> 25 g;
-    <b>carbohydrates</b> 9 g;
-    <b>potassium</b> 451 mg;`, "../img/cocoa-spiced beef tenderloin.png"],
-    ["Stir-Fried Orange Beef",
-     "Tangy orange-flavored beef with crips vegetables.<br/>&nbsp;", 
-     "$69.99", 
-     `Each serving provides:<br/>
-    <b>calories</b> 261;
-    <b>total fat</b> 9 g;
-    <b>saturated fat</b> 2 g;
-    <b>cholesterol</b> 28 mg;
-    <b>sodium</b> 418 mg;
-    <b>total fiber</b> 3 g;
-    <b>protein</b> 23 g;
-    <b>carbohydrates</b> 23 g;
-    <b>potassium</b> 648 mg;`, "../img/cocoa-spiced beef tenderloin.png"],
-    ["Sweet-and-Sour Chicken", 
-     "Sweet and sour flavours make a winning combination in this healthier version of a popular Chinese dish.", 
-     "$39.99",
-     `Each serving provides:<br/>
-    <b>calories</b> 221;
-    <b>total fat</b> 6 g;
-    <b>saturated fat</b> 1 g;
-    <b>cholesterol</b> 51 mg;
-    <b>sodium</b> 287 mg;
-    <b>total fiber</b> 3 g;
-    <b>protein</b> 23 g;
-    <b>carbohydrates</b> 21 g;
-    <b>potassium</b> 460 mg;`, "../img/cocoa-spiced beef tenderloin.png"]
-]
+let cart = []
 
 const dateClicked = e => {
     let date = e.target.dataset.date
     e.target.style.color = "black"
-    // e.target.classList.add("hello")
-    // selectDate.forEach(f => f.classList.remove('hello'));
     e.target.classList.toggle("clicked");
 
     if (e.target.classList.contains("clicked")) {
         const menuCard = () => {
-            menu.forEach(p => {
+            meals.forEach((meal) => {
                 mealGroup.innerHTML += `
-                
                 <div class="card border border-secondary">
-                        <img src="${p[4]}" class="card-img-top" alt="${p[0]}">
+                        <img src="${meal.imgSrc}" class="card-img-top" alt="${meal.name}">
                     <div class="card-body">
-                        <h5 class="card-title">${p[0]}</h5>
-                        <p class="card-text">${p[1]}</p>
+                        <h5 class="card-title">${meal.name}</h5>
+                        <p class="card-text">${meal.description}</p>
                     </div>
                         <ul class="list-group list-group-flush">
-                        <li class="list-group-item small">Price: <span class="fw-bold" id="price">${p[2]}</span></li>
-                        <li class="list-group-item small">${p[3]}
+                        <li class="list-group-item small">Price: <span class="fw-bold" id="price">${meal.price}</span></li>
+                        <li class="list-group-item small">${meal.facts}
                         </li>
                         </ul>
                     <div class="card-body">
-                        <label for="quantity">Quantity: </label>
-                        <input type="number" class="small" id="quantity" min="1">
+                        <div class="units">
+                            <div class="btn add-Cart" onclick="addToCart(${meal.id})">Add to cart</div>
+                            <div class="btn minus onclick="changeNumberOfUnits('minus', ${meal.id})">-</div>
+                            <div id="quantity" class="number">0</div>
+                            <div class="btn plus onclick="changeNumberOfUnits('plus', ${meal.id})">+</div>           
+                        </div>
                     </div>
-                </div>`
+                </div>
+                `
             })
+            
         }
         e.target.classList.add("delete")
 
@@ -92,16 +56,76 @@ const dateClicked = e => {
         main.appendChild(mealGroup)
         mealGroup.appendChild(menuCard())
         
-
-       
-
     } else if (e.target.classList.contains("delete")) {
             var delMain = document.querySelector(".deleteID");
             delMain.remove();
-            // main.remove()
     }
- 
 }
+
+
+// ADD CART
+function addToCart(id) {
+    if(cart.some((item) => item.id === id)) {
+        alert("boo")
+    } else {
+        const item = meals.find((meal) => meal.id === id)
+        cart.push({
+            ...item,
+            numberOfUnits: 1,
+        })
+    }
+    updateCart()
+
+    console.log(cart)
+}
+
+function updateCart() {
+    renderCart()
+}
+
+function renderCart() {
+    cartContainer.innerHTML = ""
+    cart.forEach((meal) => {
+        cartContainer.innerHTML += `
+                <div class="card border border-secondary">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.name}</h5>
+                    </div>
+                        <p class="list-group-item small">Price: <span class="fw-bold" id="price">${meal.price}</p>
+                    <div class="card-body">
+                        <div class="units">
+                            <div class="number">${meal.numberOfUnits}</div>
+                        </div>
+                    </div>
+                </div>
+                `
+    })
+}
+
+
+function changeNumberOfUnits(action, id) {
+
+    console.log(cart)
+
+    cart = cart.map((meal) => {
+
+        let numberOfUnits = meal.numberOfUnits
+
+        if(meal.id === id) {
+            if (action === "minus") {
+                numberOfUnits--
+            } else if (action === "plus") {
+                numberOfUnits++
+            }
+        }
+        return {
+            ...item,
+            numberOfUnits,
+        }
+    })
+    updateCart()
+}
+
 
 for (let x of selectDate) {
     x.addEventListener("click", dateClicked)
---
+++ js/meals.js
@@ -0,0 +1,70 @@
+const meals = [
+    {
+        id: 0,
+        name: "Cocoa-spiced Beef Tenderloin",
+        description: "Latin American flavors come alive in this festive beef dish with fruity salsa.",
+        price: 399.90,
+        facts: `Each serving provides:<br/>
+        <b>calories</b> 215;
+        <b>total fat</b> 9 g;
+        <b>saturated fat</b> 3 g;
+        <b>cholesterol</b> 67 mg;
+        <b>sodium</b> 226 mg;
+        <b>total fiber</b> 2 g;
+        <b>protein</b> 25 g;
+        <b>carbohydrates</b> 9 g;
+        <b>potassium</b> 451 mg;`,
+        imgSrc: "../img/beef-tenderloin.png",
+    },
+    {
+        id: 1,
+        name: "Turkey Club Burger",
+        description: "Lighten up your traditional hamburger with lean ground turkey--less saturated fat, without less flavor.",
+        price: 339,
+        facts: `Each serving provides:<br/>
+        <b>calories</b> 299;
+        <b>total fat</b> 11 g;
+        <b>saturated fat</b> 2 g;
+        <b>cholesterol</b> 89 mg;
+        <b>sodium</b> 393 mg;
+        <b>total fiber</b> 5 g;
+        <b>protein</b> 29 g;
+        <b>carbohydrates</b> 26 g;
+        <b>potassium</b> 424 mg;`,
+        imgSrc: "../img/turkey-club-burger.jpg",
+    },
+    {
+        id: 2,
+        name: "Grilled Tuna and Spinach Salad",
+        description: "Tuna is a sturdy fish that is easy on the stomach.",
+        price: 449.50,
+        facts: `Each serving provides:<br/>
+        <b>calories</b> 282;
+        <b>total fat</b> 10 g;
+        <b>saturated fat</b> 2 g;
+        <b>cholesterol</b> 42 mg;
+        <b>sodium</b> 418 mg;
+        <b>total fiber</b> 5 g;
+        <b>protein</b> 31 g;
+        <b>carbohydrates</b> 15 g;
+        <b>potassium</b> 874 mg;`,
+        imgSrc: "../img/tuna.jpg",
+    },
+    {
+        id: 3,
+        name: "Cold Fusilli Pasta",
+        description: "A whole new twist on pasta salad!",
+        price: 280,
+        facts: `Each serving provides:<br/>
+        <b>calories</b> 418;
+        <b>total fat</b> 11 g;
+        <b>saturated fat</b> 3 g;
+        <b>cholesterol</b> 10 mg;
+        <b>sodium</b> 455 mg;
+        <b>total fiber</b> 13 g;
+        <b>protein</b> 21 g;
+        <b>carbohydrates</b> 63 g;
+        <b>potassium</b> 576 mg;`,
+        imgSrc: "../img/pasta.jpg",
+    }
+]
